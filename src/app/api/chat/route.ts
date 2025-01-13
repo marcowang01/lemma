d0c2edcf5cc00f65ed7dc901eb2f60ca94c7ceee
marcowang01 <marcowang01@gmail.com>
2025-01-13 11:37:36 -0600

    cleanup

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,14 +1,16 @@
 import { getSystemPrompt } from "@/lib/prompts"
 import { WolframAlphaTool } from "@langchain/community/tools/wolframalpha"
+import { BaseLanguageModelInput } from "@langchain/core/language_models/base"
 import {
-  AIMessage,
   AIMessageChunk,
+  BaseMessage,
   HumanMessage,
   SystemMessage,
   ToolMessage,
 } from "@langchain/core/messages"
+import { Runnable } from "@langchain/core/runnables"
 import { concat } from "@langchain/core/utils/stream"
-import { ChatOpenAI } from "@langchain/openai"
+import { ChatOpenAI, ChatOpenAICallOptions } from "@langchain/openai"
 import { z } from "zod"
 
 export const maxDuration = 60
@@ -40,64 +42,15 @@ export async function POST(req: Request) {
 
   const llmWithTools = llm.bindTools([wolframAlphaTool])
 
-  const conversation = [new SystemMessage(getSystemPrompt()), new HumanMessage(userPrompt)]
-
-  async function* handleLLMStream() {
-    while (true) {
-      const iterator = await llmWithTools.stream(conversation)
-      let gathered: AIMessageChunk | undefined = undefined
-
-      for await (const chunk of iterator) {
-        console.log(`Chunk: ${JSON.stringify(chunk, null, 2)}`)
-
-        // Stream content back to the client
-        if (chunk.content) {
-          yield chunk.content
-        }
-        gathered = gathered !== undefined ? concat(gathered, chunk) : chunk
-      }
-
-      // Finalize AI message with accumulated content
-      if (gathered) {
-        conversation.push(gathered)
-      }
-
-      if (!gathered?.tool_calls || gathered?.tool_calls?.length === 0) {
-        // No more tool calls, stop iteration
-        console.log("No more tool calls, stopping iteration")
-        break
-      }
-
-      console.log(`Tool calls: ${JSON.stringify(gathered?.tool_calls)}`)
-
-      // Process each tool call
-      for (const toolCall of gathered.tool_calls) {
-        if (toolCall.name === "wolfram-alpha") {
-          try {
-            const toolMessage = (await wolframAlphaTool.invoke(toolCall)) as ToolMessage
-
-            console.log(
-              `Tool message: ${JSON.stringify(toolMessage)}, tool call: ${JSON.stringify(toolCall)}`
-            )
-
-            conversation.push(toolMessage)
-            yield `<span style="color: green;">Tool result: ${toolMessage.content}</span>`
-          } catch (error) {
-            console.error("Tool invocation error:", error)
-            yield `<span style="color: red;">Tool invocation failed: ${String(error)}</span>`
-          }
-        } else {
-          console.error("Unknown tool call:", toolCall.name)
-          yield `<span style="color: red;">Unknown tool: ${toolCall.name}</span>`
-        }
-      }
-    }
-  }
+  const conversation: BaseMessage[] = [
+    new SystemMessage(getSystemPrompt()),
+    new HumanMessage(userPrompt),
+  ]
 
   // Convert async generator to ReadableStream
   const stream = new ReadableStream({
     async pull(controller) {
-      const iterator = handleLLMStream()
+      const iterator = handleLLMStream(conversation, llmWithTools, wolframAlphaTool)
       try {
         const { value, done } = await iterator.next()
         if (done) {
@@ -117,4 +70,69 @@ export async function POST(req: Request) {
       "Content-Type": "text/plain; charset=utf-8",
     },
   })
+}
+
+async function* handleLLMStream(
+  conversation: BaseMessage[],
+  llmWithTools: Runnable<BaseLanguageModelInput, AIMessageChunk, ChatOpenAICallOptions>,
+  wolframAlphaTool: WolframAlphaTool
+) {
+  while (true) {
+    console.log(
+      `\nConversation: ${JSON.stringify(
+        conversation.map((c) => c.getType()),
+        null,
+        2
+      )}`
+    )
+    const iterator = await llmWithTools.stream(conversation)
+    let gathered: AIMessageChunk | undefined = undefined
+
+    for await (const chunk of iterator) {
+      // Stream content back to the client
+      if (chunk.tool_calls?.length === 0 && chunk.content && chunk.getType() === "ai") {
+        console.log(`Chunk content: ${JSON.stringify(chunk, null, 2)}`)
+        yield chunk.content
+      }
+      gathered = gathered !== undefined ? concat(gathered, chunk) : chunk
+    }
+
+    console.log(`iteration done Gathered: ${JSON.stringify(gathered, null, 2)}`)
+
+    // Finalize AI message with accumulated content
+    if (gathered) {
+      // console.log(`Gathered: ${JSON.stringify(gathered, null, 2)}`)
+      conversation.push(gathered)
+    }
+
+    if (!gathered?.tool_calls || gathered?.tool_calls?.length === 0) {
+      // No more tool calls, stop iteration
+      console.log("No more tool calls, stopping iteration")
+      break
+    }
+
+    // console.log(`Tool calls: ${JSON.stringify(gathered?.tool_calls, null, 2)}`)
+
+    // Process each tool call
+    for (const toolCall of gathered.tool_calls) {
+      if (toolCall.name === "wolfram-alpha") {
+        try {
+          const toolMessage = (await wolframAlphaTool.invoke(toolCall)) as ToolMessage
+
+          // console.log(
+          //   `Tool message: ${JSON.stringify(toolMessage, null, 2)}, tool call: ${JSON.stringify(toolCall, null, 2)}`
+          // )
+
+          conversation.push(toolMessage)
+          // yield `<span style="color: green;">Tool result: ${toolMessage.content}</span>`
+        } catch (error) {
+          console.error("Tool invocation error:", error)
+          yield `<span style="color: red;">Tool invocation failed: ${String(error)}</span>`
+        }
+      } else {
+        console.error("Unknown tool call:", toolCall.name)
+        yield `<span style="color: red;">Unknown tool: ${toolCall.name}</span>`
+      }
+    }
+  }
 }
