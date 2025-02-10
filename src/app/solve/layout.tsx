f12be4114c95ad8b2bac883b02f65b0e7268d61e
marcowang01 <marcowang01@gmail.com>
2025-02-10 01:40:06 -0800

    fix lemma logo

--- src/app/solve/layout.tsx
+++ src/app/solve/layout.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link"
+
 export default function SolveLayout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <h3 className="font-appleGaramond fixed left-4 top-2.5 text-5xl font-normal italic">lemma</h3>
+      <Link
+        href="/"
+        className="font-appleGaramond fixed left-4 top-2.5 text-5xl font-normal italic"
+      >
+        lemma
+      </Link>
       {children}
     </div>
   )
